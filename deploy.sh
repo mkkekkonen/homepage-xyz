72a891dfa49238a652c22a988ebf721221cfa0bc
Maija Kekkonen <mkkekkonen@protonmail.com>
2019-09-13 22:49:24 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,15 @@ mv ../mkkekkonen.github.io/*.js.map ../mkkekkonen.github.io/old/
 mv ../mkkekkonen.github.io/*.html ../mkkekkonen.github.io/old/
 
 cp ./dist/* ../mkkekkonen.github.io/
+
+cd ../mkkekkonen.github.io/
+
+git add -A
+git commit
+
+read -p "About to push. Continue (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  git push
+else
+  echo "Aborted"
+fi
